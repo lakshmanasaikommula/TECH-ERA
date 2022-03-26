9d8cfcdc16ca000806171af8a8ebe001ee4cf300
lakshmanasaikommula <lakshman.kommula@gmail.com>
2022-03-26 12:15:16 +0000

    tech-era

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
+import Home from './components/Home'
+import NotFound from './components/NotFound'
+import CourseItemDetails from './components/CourseItemDetails'
+
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/courses/:id" component={CourseItemDetails} />
+      <Route path="/not-found" component={NotFound} />
+      <Redirect to="not-found" />
+    </Switch>
+  </BrowserRouter>
+)
+
+export default App
